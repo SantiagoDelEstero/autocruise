53ed4954c98d7a46d8e09c54b63bb36ec00a43b3
SantiagoDelEstero <55442351+SantiagoDelEstero@users.noreply.github.com>
2019-12-30 15:03:51 +0100

    Comments

--- autocruise.ino
+++ autocruise.ino
@@ -15,6 +15,7 @@
 #define PIN_APPS_FEEDBACK_1 A0
 #define PIN_APPS_FEEDBACK_2 A1
 
+// PWM outputs
 #define PIN_APPS_1 9
 #define PIN_APPS_2 10
 
@@ -22,6 +23,7 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2); 
 
+// Volatile because it's accessed from an interrupt
 volatile unsigned int wheelpulses = 0;
 unsigned int readpulses;
 byte carspeed = 0;
@@ -35,6 +37,7 @@ byte speedHistory[SPEED_HISTORY_DEPTH];
 byte i;
 
 void setup() {
+  // Modify the TCCR1B register in order to have a faster PWM frequency
   TCCR1B = (TCCR1B & B11111000) | B00000010;
   pinMode(PIN_WHEEL_SENSOR, INPUT_PULLUP);
   pinMode(PIN_RELAY, OUTPUT);
@@ -45,8 +48,9 @@ void setup() {
   pinMode(PIN_BRAKE_PEDAL, INPUT_PULLUP);
   pinMode(PIN_ENCODER_A, INPUT_PULLUP);
   pinMode(PIN_ENCODER_B, INPUT_PULLUP);
-  pinMode(PIN_APPS_1, INPUT);
-  pinMode(PIN_APPS_2, INPUT);
+  pinMode(PIN_APPS_1, OUTPUT);
+  pinMode(PIN_APPS_2, OUTPUT);
+  // Every time there is a pulse in the wheel sensor this interrupt will be called in order to calculate the speed.
   attachInterrupt(digitalPinToInterrupt(PIN_WHEEL_SENSOR), sensor, RISING);
   lcd.init();
   lcd.backlight();
@@ -57,13 +61,18 @@ void setup() {
 }
 
 void loop() {
+  // Update the speed every half second
   if(millis() - speedlastupdated >= 500){
     speedlastupdated = millis();
+    // We need an atomic bloc (interrupts disabled) in order to read the number of pulses in the last half second.
+    // This is because wheelpulses is two bytes long, and there is a chance an interrupt could happen in the middle.
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
       readpulses = wheelpulses;
       wheelpulses = 0;
     }
+    // Calculate the car speed in km/h. The constant is empirical.
     newcarspeed = round(4.0*(float)readpulses/15.0);
+    // Update the screen if the speed has changed
     if(carspeed != newcarspeed){
       lcd.setCursor(0,0);
       lcd.print(newcarspeed);
@@ -79,12 +88,15 @@ void loop() {
       }
       carspeed = newcarspeed;
     }
+    // Put the speed in the speed history. This is to see if we have been under or over the target speed for a while.
     for(i=SPEED_HISTORY_DEPTH - 1; i>0; i--){
       speedHistory[i] = speedHistory[i-1];
     }
     speedHistory[0] = carspeed;
+    // overrideAccelerator is true when the cruise control is on
     if(overrideAccelerator){
       if(carspeed < targetspeed){
+        // We apply more throttle the further away we are from the target speed
         if(carspeed <= (targetspeed - 8)){
           throttle += 2.5;
         }else if(carspeed <= (targetspeed - 5)){
@@ -92,6 +104,7 @@ void loop() {
         }else{
           throttle += 0.5;
         }
+        // If we have been under the desired speed for 4 seconds add a bit of extra throttle.
         if(timeBelow() >= 8){
           throttle += 0.5;
         }
@@ -107,21 +120,26 @@ void loop() {
           throttle -= 0.5;
         }
       }
+      // We apply the new throttle
       throttle = constrain(throttle, 0, 100);
       throttleWrite(throttle);
     }
   }
 
+  // Check for user input.
   processButtonActivate();
   processButtonActivateCS();
   processEncoder();
   
+  // If we are braking turn the cruise control off
   if(!digitalRead(PIN_BRAKE_PEDAL)){
     overrideAccelerator = false;
   }
 
   if(digitalRead(PIN_RELAY) != overrideAccelerator){
     if(overrideAccelerator){
+      // If the cruise control has just been turned on, we start by applying the driver's current throttle.
+      // Thus we avoid a shock
       throttle = throttleRead();
       throttleWrite(throttle);
       digitalWrite(PIN_RELAY, HIGH);
@@ -148,16 +166,20 @@ void loop() {
   }
 }
 
+// This function reads the driver's current throttle.
 float throttleRead(){
   return constrain(((float)analogRead(PIN_APPS_FEEDBACK_2)/4 - 19.38)/0.843, 0, 100);
 }
 
+// Makes sure every time a throttle input is given it's within the good range.
+// Otherwise the check engine light turns on. Constants are empirical.
 void throttleWrite(float t){
   t = constrain(t, 0, 100);
   analogWrite(PIN_APPS_1, round(39.27 + (float)t*1.715));
   analogWrite(PIN_APPS_2, round(19.38 + (float)t*0.843));
 }
 
+// Check if the activate button is pressed
 void processButtonActivate(){
   static unsigned long lastButtonChange = 0;
   static byte lastButtonState = HIGH;
@@ -175,6 +197,7 @@ void processButtonActivate(){
       overrideAccelerator = false;
       alreadyExecuted = true;
     }else{
+      // The button has to be pressed for 1 second in order to turn on the cruise control
       if((millis() - lastButtonChange) > 1000){
         overrideAccelerator = true;
         alreadyExecuted = true;
@@ -183,6 +206,7 @@ void processButtonActivate(){
   }
 }
 
+// Check if the activate to current speed button is pressed
 void processButtonActivateCS(){
   static unsigned long lastButtonChange = 0;
   static byte lastButtonState = HIGH;
@@ -209,6 +233,7 @@ void processButtonActivateCS(){
   }
 }
 
+// Read the changes in the encoder. This sets the new target speed
 void processEncoder(){
   static boolean alreadyExecuted = false;
   byte readingA, readingB;
@@ -241,6 +266,7 @@ byte nofdigits(byte number){
   }
 }
 
+// Time above the target speed
 byte timeAbove(){
   byte t = 0;
   for(i=0; i<SPEED_HISTORY_DEPTH; i++){
@@ -253,6 +279,7 @@ byte timeAbove(){
   return t;
 }
 
+// Time below the target speed
 byte timeBelow(){
   byte t = 0;
   for(i=0; i<SPEED_HISTORY_DEPTH; i++){
@@ -265,6 +292,7 @@ byte timeBelow(){
   return t;
 }
 
+// Increment wheelpulses at every pulse of the wheel sensor
 void sensor(){
   wheelpulses++;
 }
